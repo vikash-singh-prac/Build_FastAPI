1addb6fcb6e537be6f8c54013e59396cd26b0036
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-02-18 19:12:14 -0500

    Create GET response

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world!!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your post!"}
